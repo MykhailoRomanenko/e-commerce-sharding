a36744551da2db6ff7ef82dcdca648a52dd467a1
MykhailoRomanenko <moromanenko@gmail.com>
2023-04-28 10:29:09 +0300

    fix supplier module

--- data-access-service/src/supplier/supplier.module.ts
+++ data-access-service/src/supplier/supplier.module.ts
@@ -1,8 +1,15 @@
 import { Module } from '@nestjs/common';
 import { SupplierController } from './supplier.controller';
 import { SupplierService } from './supplier.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Supplier, SupplierSchema } from './entities/supplier.entity';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([
+      { name: Supplier.name, schema: SupplierSchema },
+    ]),
+  ],
   controllers: [SupplierController],
   providers: [SupplierService],
 })
