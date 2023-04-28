9f0f021a1a2e2d7e394d0e7fbb2830ddc5bf60cc
MykhailoRomanenko <moromanenko@gmail.com>
2023-04-28 09:44:10 +0300

    fix cache

--- gateway-service/src/modules/category/category.module.ts
+++ gateway-service/src/modules/category/category.module.ts
@@ -3,9 +3,10 @@ import { CategoryService } from './category.service';
 import { CategoryController } from './category.controller';
 import { ConfigModule } from '@nestjs/config';
 import { TransportModule } from '../transport/transport.module';
+import { CacheModule } from '@nestjs/cache-manager';
 
 @Module({
-  imports: [ConfigModule, TransportModule],
+  imports: [ConfigModule, TransportModule, CacheModule.register()],
   controllers: [CategoryController],
   providers: [CategoryService],
 })
